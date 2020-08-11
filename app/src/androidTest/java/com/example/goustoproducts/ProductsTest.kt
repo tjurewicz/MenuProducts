6566d9e3878af4bd866d59073215f018ee197588
WildcardTJ <tomjurewicz@hotmail.co.uk>
2020-08-11 18:21:59 +0100

    added additional checks to UI tests

--- app/src/androidTest/java/com/example/goustoproducts/ProductsTest.kt
+++ app/src/androidTest/java/com/example/goustoproducts/ProductsTest.kt
@@ -9,12 +9,14 @@ import androidx.test.espresso.action.GeneralLocation
 import androidx.test.espresso.action.Press
 import androidx.test.espresso.action.Tap
 import androidx.test.espresso.action.ViewActions.*
+import androidx.test.espresso.assertion.ViewAssertions.doesNotExist
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.contrib.RecyclerViewActions
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.rule.ActivityTestRule
 import com.example.goustoproducts.ui.products.ProductListAdapter
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -25,38 +27,59 @@ class ProductsTest {
     @get:Rule
     var activityRule: ActivityTestRule<MainActivity> = ActivityTestRule(MainActivity::class.java)
 
+    @Before
+    fun setup() {
+        // Making sure that the list renders before the test checks for views
+        Thread.sleep(2000)
+    }
+
     @Test
     fun clickingOnItemInListOpensDetailView() {
 
-        Thread.sleep(1500)
         val productTitle = "Borsao Macabeo"
+        val productPrice = "£6.95"
         val productDescription =
             "A flavoursome Summer wine made from the indigenous Macabeo grape in northern Spain. A balanced, modern white with flavours of ripe peach, zesty lemon and nutty undertones, it leaves the palate with a clean and fruity finish."
+
+        onView(withId(R.id.product_recycler_view)).check(matches(hasDescendant(withText(productTitle))))
+        onView(withId(R.id.product_recycler_view)).check(matches(hasDescendant(withText(productPrice))))
+        onView(withText(productDescription)).check(doesNotExist())
+
         onView(withId(R.id.product_recycler_view))
             .perform(
                 RecyclerViewActions.actionOnItem<ProductListAdapter.ViewHolder>(
                     hasDescendant(withText(productTitle)), click()
                 )
             );
+
         onView(withId(R.id.detail_title)).check(matches(withText(productTitle)))
         onView(withId(R.id.detail_description)).check(matches(withText(productDescription)))
+        onView(withText(productPrice)).check(doesNotExist())
     }
 
     @Test
     fun clickingOnBackButtonInDetailViewReturnsToListView() {
 
-        Thread.sleep(1500)
         val productTitle = "Borsao Macabeo"
+        val productPrice = "£6.95"
+
         onView(withId(R.id.product_recycler_view)).check(matches(hasDescendant(withText(productTitle))))
+        onView(withId(R.id.product_recycler_view)).check(matches(hasDescendant(withText(productPrice))))
+
         onView(withId(R.id.product_recycler_view))
             .perform(
                 RecyclerViewActions.actionOnItem<ProductListAdapter.ViewHolder>(
                     hasDescendant(withText(productTitle)), click()
                 )
             );
+
         onView(withId(R.id.detail_title)).check(matches(withText(productTitle)))
+        onView(withText(productPrice)).check(doesNotExist())
+
         onView(withId(R.id.back_button)).perform(click())
+
         onView(withId(R.id.product_recycler_view)).check(matches(hasDescendant(withText(productTitle))))
+        onView(withId(R.id.product_recycler_view)).check(matches(hasDescendant(withText(productPrice))))
     }
 
     @Test
@@ -66,8 +89,7 @@ class ProductsTest {
         val productTitle = "Dolci Colline Prosecco (200ml bottle)"
         val productPrice = "£3.99"
 
-        Thread.sleep(1500)
-
+        onView(withText(productTitle)).check(doesNotExist())
         onView(withId(R.id.product_search))
             .perform(clickSearch())
             .perform(typeText(searchText), closeSoftKeyboard())
@@ -78,6 +100,7 @@ class ProductsTest {
     }
 
     private fun clickSearch(): ViewAction {
+        // custom click function as the standard one was clicking in the wrong place
         return actionWithAssertions(
             GeneralClickAction(
                 Tap.SINGLE,
